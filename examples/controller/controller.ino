2202b7041e800be2d7154d010b437820e85ecb13
mohake-keshavarzi <mohake.keshavarzi@gmail.com>
2023-03-26 16:04:18 +0330

    write single coil tested and works

--- examples/controller/controller.ino
+++ examples/controller/controller.ino
@@ -7,13 +7,14 @@
 boolean coils[NUM_OF_COILS] {};
 
 
-Slave miniModbusSlave{MY_ID,Serial};
+Slave miniModbusSlave{MY_ID,Serial2};
 
 // ModbusRequestResponseParser parser(MY_ID
 // Based on Uno
 void setup()
 {
     Serial.begin(19200);
+    Serial2.begin(19200);
     pinMode(13, OUTPUT);
 
     miniModbusSlave.setCoilsRefrence(coils,NUM_OF_COILS);
@@ -23,6 +24,12 @@ void setup()
 void loop()
 {
     miniModbusSlave.execute();
+    Serial.print("Coil0:");
+    Serial.println(coils[0]);
+    Serial.print("Coil1:");
+    Serial.println(coils[1]);
+    
     digitalWrite(13,coils[0]);
+    
    
 }
--- examples/master/master.ino
+++ examples/master/master.ino
@@ -1,14 +1,28 @@
 #include "MiniModbus.h"
 
+#define CONTROLLER_ID 0x01
 
+Master miniModbusMaster{Serial};
+boolean taskIsDone{false};
 
 void setup() {
-  
-     
+//    Serial2.begin(19200);
+    Serial.begin(19200);
+    pinMode(13, OUTPUT);
+//    pinMode(20, INPUT_PULLUP);
+
+    
 }
 void loop() {
- 
+    boolean values[2]={false,true};
+//    digitalWrite(13,values[1]);
+    if(miniModbusMaster.writeSingleCoil(CONTROLLER_ID,0x0000,true)){
+        digitalWrite(13,true);
+    }
+    else{
+        digitalWrite(13,false);
+    }
+    delay(500);
+
+    
 }
-
-
-
